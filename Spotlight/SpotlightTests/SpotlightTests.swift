7964624cb549b25d44fd2efa1fa8e6713d0d6c95
yg626 <yg626@cornell.edu>
2024-11-25 00:00:39 -0500

    xcodeproj

---
+++ Spotlight/SpotlightTests/SpotlightTests.swift
@@ -0,0 +1,16 @@
+//
+//  SpotlightTests.swift
+//  SpotlightTests
+//
+//  Created by Yuan Gao on 11/24/24.
+//
+
+import Testing
+
+struct SpotlightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

fdb08a14116afc7b4feb28a62c5787bc6865e867
yg626 <yg626@cornell.edu>
2024-11-25 01:36:53 -0500

    okay everything is in one file

---
+++ Spotlight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotlight
+//
+//  Created by Yuan Gao on 11/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

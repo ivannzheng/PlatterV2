ed123c3c33227e9db8f29bd8a5b0b07a2dbc55e1
yg626 <yg626@cornell.edu>
2024-11-25 03:11:29 -0500

    FINAL INITIAL SET UP FOR APP FR

--- Spotlight/App/ViewController.swift
+++ Spotlight/App/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - Properties (view)
+    
+    // MARK: - Properties (data)
+    
+    // MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
     }
-
-
+    
+    // MARK: - Set Up Views
+    
+    
+    // MARK: - UICollectionViewDelegate
+    
+    // MARK: - UICollectionViewDataSource
+    
+    // MARK: - UICollectionViewDelegateFlowLayout
+    
+    
 }
 

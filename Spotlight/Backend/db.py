9cc62aaeb6a292b58534e509c20dfbf25a29250e
ivanzheng <ivannzheng@gmail.com>
2024-11-25 22:04:31 -0500

    added a users_dao.py

--- Spotlight/Backend/db.py
+++ Spotlight/Backend/db.py
@@ -1,3 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class User(db.model):
+    """
+    User Model
+    """
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+
+
